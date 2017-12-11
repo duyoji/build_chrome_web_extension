fe88b38bf33f6620d2253412abc89f2232abd0fb
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2017-12-11 22:00:38 +0900

    Added youtube list.

--- popup.js
+++ popup.js
@@ -1,15 +1,31 @@
-let a = 0;
 (() => {
-  a++;
-  console.log(a);
-  // const alertLoop = () => {
-  //   setTimeout(function alertLoop() {
-  //     alert(1);
-  //     // alertLoop();
-  //     chrome.tabs.create({}, (tab) => {
-  //       console.log(tab);
-  //     });
-  //   }, 0);
-  // };
-  // alertLoop();
+  const playButton = document.querySelector('#play-button');
+  const input = document.querySelector('#youtube-id');
+  const youtubeIframe = document.querySelector('#youtube-iframe');
+  const youtubeSelect = document.querySelector('#youtube-select');
+  const selectOptions = {
+    'OvNkaFHSAXY': 'Arctic Monkeys - When The Sun Goes Down / Brianstorm',
+    'yWP6Qki8mWc': `The Beatles "Help" Live 1965 (Reelin' In The Years Archives)`,
+    'i_2mWhfOhGU': 'Oasis - Live Forever - Official Video'
+  };
+  const playWithId = (youtubeId) => {
+    if(!youtubeId) return;
+    const newYoutubeSrc = `https://www.youtube.com/embed/${youtubeId}?autoplay=1`;
+    youtubeIframe.src = newYoutubeSrc;
+  };
+
+  for(let youtubeId in selectOptions) {
+    const opt = document.createElement('option');
+    opt.value = youtubeId;
+    opt.innerHTML = selectOptions[youtubeId];
+    youtubeSelect.appendChild(opt);
+  }
+
+  youtubeSelect.addEventListener('change', (event) => {
+    console.log(event, '@@@@@@@@@@');
+    input.value = event.target.value;
+  });
+  playButton.addEventListener('click', (event) => {
+    playWithId(input.value);
+  });
 })();
